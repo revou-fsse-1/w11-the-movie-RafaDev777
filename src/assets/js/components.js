504a954a0f271fafad3ae4065a50c67addbadb84
Rafael Pradita_PC <rafaeljonathan.work@gmail.com>
2023-04-05 15:58:20 +0700

    component and API test preparation

---
+++ src/assets/js/components.js
@@ -0,0 +1,5 @@
+export const movieCard = document.createElement('div');
+movieCard.className('.card');
+movieCard.innerHTML=`
+<img src="${movie.image}" class=""/>
+`;
---
+++ src/assets/js/dashboard.js
@@ -0,0 +1,5 @@
+import { movieCard } from "./components.js";
+import { API_ENDPOINT } from "./constants.js";
+
+let x = document.querySelector(".card-container")
+x.appendChild(movieCard)
