aea3453b8acfc7c59ba34fa40ce457a4e976fdc2
Rafael Pradita_PC <rafaeljonathan.work@gmail.com>
2023-04-06 12:26:50 +0700

    adding container to each page, update all page styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: "390px",
-      lg: "1280px",
-    },
-
     fontFamily: {
       sans: ["roboto", "sans-serif"],
       display: ["poppins", "sans-serif"],
@@ -14,6 +9,11 @@ module.exports = {
       colors: {
         primary: "#37C6F3",
       },
+      screens:{
+        sm: "390px",
+      lg: "1280px",
+
+      }
     },
   },
   plugins: [],
